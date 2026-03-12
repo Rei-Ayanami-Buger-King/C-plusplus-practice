7af3f55e34d28b9ad7a1ffac0680692a58b7ad84
Rei Ayanami <128055689+Rei-Ayanami-Buger-King@users.noreply.github.com>
2026-03-12 15:11:49 -0500

    comments
    string formatting

--- main,.cpp
+++ main,.cpp
@@ -1,12 +1,13 @@
 using namespace std;
 #include <iostream>
-
+#include <iomanip> // lets me set the precision on a float output using "setprecision"
 //Today I will learn how to use functions
 
 //the void means I don't want the function to return anything (that doesn't mean I can't print out of it or call another function from it)
 void output(int amount, float price, string name){
-    cout << "You bought " << amount << " " << name << "s and they cost $" << price <<  " each\n In total you owe $" << price * amount; 
+    cout << fixed << setprecision(2) << "You bought " << amount << " " << name << "s and they cost $" << price <<  " each\n In total you owe $" << price * amount; //the fixed stops it from outputting scientific notation
 }
+//we can also use printf() to format the float/string output
 //output needs to be above input due to scope reasoning
 void input(){
     string name;
